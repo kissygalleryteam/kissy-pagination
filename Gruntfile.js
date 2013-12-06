da5d37a6af866397ddda957f0ad223ec97296f7a
jilei.liu <jilei.liu@alibaba-inc.com>
2013-12-06 16:53:18 +0800

    build

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,6 +32,10 @@ module.exports = function(grunt) {
                     {
                         src: "<%= pkg.version %>/index.js",
                         dest: "<%= pkg.version %>/build/index.js"
+                    },
+                    {
+                        src: "<%= pkg.version %>/src/style.css",
+                        dest: "<%= pkg.version %>/build/src/"
                     }
                 ]
             }
